b3e4f848ffb32da96aecc85ea8dafa4c4e1d8496
altaf-khan-jk <akhan8508@conestogac.on.ca>
2025-12-05 21:22:55 +0000

    updated the front end part with docker file and all the required data

--- frontend/shell-app/src/App.js
+++ frontend/shell-app/src/App.js
@@ -2,12 +2,25 @@ import React from "react";
 
 function App() {
   const path = window.location.pathname;
+
   return (
     <div>
       <h1>ObsShop Shell</h1>
-      <nav><a href="/">Home</a> | <a href="/products">Products</a></nav>
+
+      <nav>
+        <a href="/">Home</a> | <a href="/products">Products</a>
+      </nav>
+
       <div>
-        {path === "/products" ? <iframe src="http://localhost:3001/" style={{width:"100%",height:"80vh"}} /> : <p>Welcome</p>}
+        {path === "/products" ? (
+          <iframe
+            src="http://localhost:3001/"
+            title="Products Microfrontend"
+            style={{ width: "100%", height: "80vh" }}
+          />
+        ) : (
+          <p>Welcome</p>
+        )}
       </div>
     </div>
   );
